c156607a751b0b38d4d4b73126594c00b32b8908
Brian L. Troutwine <brian@troutwine.us>
2016-11-07 07:18:26 -0800

    Introduce benchmarks for metric overlay
    We're kind of getting killed having to continually clone keys and
    values in metric maps. This gets especially bad when we start to
    integrate additional languages whose memory models require
    copying.
    
    This commit adds benchmarks to help pin down the work needed to
    repair the situation before it gets out of hand.
    
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- benches/metrics.rs
+++ benches/metrics.rs
@@ -5,7 +5,29 @@ extern crate cernan;
 
 use self::test::Bencher;
 
-use cernan::metric::{Metric};
+use cernan::metric::{Metric,TagMap};
+
+#[bench]
+fn bench_overlay_tags_from_map(b: &mut Bencher) {
+    let mut tag_map = TagMap::default();
+    tag_map.insert("foo".into(), "bar".into());
+    tag_map.insert("oof".into(), "rab".into());
+    b.iter(|| {
+        let mut m = Metric::new("foo", 1.1).overlay_tag("foo", "22");
+        m.overlay_tags_from_map(&tag_map);
+    });
+}
+
+#[bench]
+fn bench_merge_tags_from_map(b: &mut Bencher) {
+    let mut tag_map = TagMap::default();
+    tag_map.insert("foo".into(), "bar".into());
+    tag_map.insert("oof".into(), "rab".into());
+    b.iter(|| {
+        let mut m = Metric::new("foo", 1.1).overlay_tag("foo", "22");
+        m.merge_tags_from_map(&tag_map);
+    });
+}
 
 #[bench]
 fn bench_statsd_incr_gauge_no_sample(b: &mut Bencher) {
