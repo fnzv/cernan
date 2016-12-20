bae6b3f3ad3caf530aabec721e8c21cf974d1cb1
Brian L. Troutwine <blt@postmates.com>
2016-12-20 15:32:58 -0800

    Faster WF, WIP
    Signed-off-by: Brian L. Troutwine <blt@postmates.com>

--- src/sink/wavefront.rs
+++ src/sink/wavefront.rs
@@ -1,7 +1,6 @@
 use std::net::TcpStream;
-use std::fmt::Write;
 use std::io::Write as IoWrite;
-use metric::{Metric, LogLine, TagMap, MetricKind};
+use metric::{Metric, LogLine, TagMap};
 use buckets::Buckets;
 use sink::{Sink, Valve};
 use std::net::ToSocketAddrs;
@@ -12,8 +11,7 @@ pub struct Wavefront {
     port: u16,
     aggrs: Buckets,
     delivery_attempts: u32,
-    sink_name: String,
-    global_tags: TagMap,
+    pub stats: String,
 }
 
 #[derive(Debug)]
@@ -26,16 +24,19 @@ pub struct WavefrontConfig {
 }
 
 #[inline]
-fn fmt_tags(tags: &TagMap) -> String {
-    let mut s = String::new();
+fn fmt_tags(tags: &TagMap, s: &mut String) -> () {
     let mut iter = tags.iter();
     if let Some(&(ref fk, ref fv)) = iter.next() {
-        write!(s, "{}={}", fk, fv).unwrap();
+        s.push_str(fk);
+        s.push_str("=");
+        s.push_str(fv);
         for &(ref k, ref v) in iter {
-            write!(s, " {}={}", k, v).unwrap();
+            s.push_str(" ");
+            s.push_str(k);
+            s.push_str("=");
+            s.push_str(v);
         }
     }
-    s
 }
 
 impl Wavefront {
@@ -45,257 +46,84 @@ impl Wavefront {
             port: config.port,
             aggrs: Buckets::new(config.bin_width),
             delivery_attempts: 0,
-            sink_name: config.config_path,
-            global_tags: config.tags,
+            stats: String::with_capacity(8_192),
         }
     }
 
     /// Convert the buckets into a String that
     /// can be sent to the the wavefront proxy
-    pub fn format_stats(&mut self, now: i64) -> String {
-        let mut stats = String::new();
-        let mut payload_size: u32 = 0;
-        let mut gauge_payload_size: u32 = 0;
-        let mut delta_gauge_payload_size: u32 = 0;
-        let mut raw_payload_size: u32 = 0;
-        let mut counter_payload_size: u32 = 0;
-        let mut timer_payload_size: u32 = 0;
-        let mut histogram_payload_size: u32 = 0;
-        let mut time_spreads = Metric::new(format!("cernan.{}.time_spreads", self.sink_name), 0.0)
-            .histogram();
-        let mut counter_time_spreads = Metric::new(format!("cernan.{}.counter.time_spreads",
-                                                           self.sink_name),
-                                                   0.0)
-            .histogram();
-        let mut gauge_time_spreads =
-            Metric::new(format!("cernan.{}.gauge.time_spreads", self.sink_name), 0.0).histogram();
-        let mut delta_gauge_time_spreads =
-            Metric::new(format!("cernan.{}.delta_gauge.time_spreads", self.sink_name), 0.0).histogram();
-        let mut raw_time_spreads =
-            Metric::new(format!("cernan.{}.raw.time_spreads", self.sink_name), 0.0).histogram();
-        let mut timer_time_spreads =
-            Metric::new(format!("cernan.{}.timer.time_spreads", self.sink_name), 0.0).histogram();
-        let mut histogram_time_spreads = Metric::new(format!("cernan.{}.histogram.time_spreads",
-                                                             self.sink_name),
-                                                     0.0)
-            .histogram();
+    pub fn format_stats(&mut self, _: i64) -> () {
+        let flat_aggrs = self.aggrs
+            .counters()
+            .iter()
+            .chain(self.aggrs.gauges().iter())
+            .chain(self.aggrs.delta_gauges().iter())
+            .chain(self.aggrs.raws().iter());
 
-        for kind in &[MetricKind::Counter,
-                      MetricKind::Gauge,
-                      MetricKind::DeltaGauge,
-                      MetricKind::Timer,
-                      MetricKind::Histogram,
-                      MetricKind::Raw] {
-            match kind {
-                &MetricKind::Counter => {
-                    for (key, vals) in self.aggrs.counters().iter() {
-                        for m in vals {
-                            if let Some(v) = m.value() {
-                                payload_size = payload_size.saturating_add(1);
-                                counter_payload_size = counter_payload_size.saturating_add(1);
-                                time_spreads =
-                                    time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                counter_time_spreads = counter_time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                write!(stats, "{} {} {} {}\n", key, v, m.time, fmt_tags(&m.tags))
-                                    .unwrap();
-                            }
-                        }
-                    }
-                }
-                &MetricKind::Gauge => {
-                    for (key, vals) in self.aggrs.gauges().iter() {
-                        for m in vals {
-                            if let Some(v) = m.value() {
-                                payload_size = payload_size.saturating_add(1);
-                                gauge_payload_size = gauge_payload_size.saturating_add(1);
-                                time_spreads =
-                                    time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                gauge_time_spreads = gauge_time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                write!(stats, "{} {} {} {}\n", key, v, m.time, fmt_tags(&m.tags))
-                                    .unwrap();
-                            }
-                        }
-                    }
-                }
-                &MetricKind::DeltaGauge => {
-                    for (key, vals) in self.aggrs.delta_gauges().iter() {
-                        for m in vals {
-                            if let Some(v) = m.value() {
-                                payload_size = payload_size.saturating_add(1);
-                                delta_gauge_payload_size = delta_gauge_payload_size.saturating_add(1);
-                                time_spreads =
-                                    time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                delta_gauge_time_spreads = delta_gauge_time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                write!(stats, "{} {} {} {}\n", key, v, m.time, fmt_tags(&m.tags))
-                                    .unwrap();
-                            }
-                        }
-                    }
-                }
-                &MetricKind::Raw => {
-                    for (key, vals) in self.aggrs.raws().iter() {
-                        for m in vals {
-                            if let Some(v) = m.value() {
-                                payload_size = payload_size.saturating_add(1);
-                                raw_payload_size = raw_payload_size.saturating_add(1);
-                                time_spreads =
-                                    time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                raw_time_spreads = raw_time_spreads.insert_value((m.created_time - now).abs() as f64);
-                                write!(stats, "{} {} {} {}\n", key, v, m.time, fmt_tags(&m.tags))
-                                    .unwrap();
-                            }
-                        }
-                    }
-                }
-                &MetricKind::Timer => {
-                    for (key, hists) in self.aggrs.timers().iter() {
-                        for hist in hists {
-                            for tup in &[("min", 0.0),
-                                         ("max", 1.0),
-                                         ("2", 0.02),
-                                         ("9", 0.09),
-                                         ("25", 0.25),
-                                         ("50", 0.5),
-                                         ("75", 0.75),
-                                         ("90", 0.90),
-                                         ("91", 0.91),
-                                         ("95", 0.95),
-                                         ("98", 0.98),
-                                         ("99", 0.99),
-                                         ("999", 0.999)] {
-                                let stat: &str = tup.0;
-                                let quant: f64 = tup.1;
-                                write!(stats,
-                                       "{}.{} {} {} {}\n",
-                                       key,
-                                       stat,
-                                       hist.query(quant).unwrap(),
-                                       hist.time,
-                                       fmt_tags(&hist.tags))
-                                    .unwrap()
-                            }
-                            payload_size = payload_size.saturating_add(1);
-                            timer_payload_size = timer_payload_size.saturating_add(1);
-                            time_spreads =
-                                time_spreads.insert_value((hist.created_time - now).abs() as f64);
-                            timer_time_spreads = timer_time_spreads.insert_value((hist.created_time - now).abs() as f64);
-                            let count = hist.count();
-                            write!(stats,
-                                   "{}.count {} {} {}\n",
-                                   key,
-                                   count,
-                                   hist.time,
-                                   fmt_tags(&hist.tags))
-                                .unwrap();
-                        }
-                    }
-                }
-                &MetricKind::Histogram => {
-                    for (key, hists) in self.aggrs.histograms().iter() {
-                        for hist in hists {
-                            for tup in &[("min", 0.0),
-                                         ("max", 1.0),
-                                         ("2", 0.02),
-                                         ("9", 0.09),
-                                         ("25", 0.25),
-                                         ("50", 0.5),
-                                         ("75", 0.75),
-                                         ("90", 0.90),
-                                         ("91", 0.91),
-                                         ("95", 0.95),
-                                         ("98", 0.98),
-                                         ("99", 0.99),
-                                         ("999", 0.999)] {
-                                let stat: &str = tup.0;
-                                let quant: f64 = tup.1;
-                                write!(stats,
-                                       "{}.{} {} {} {}\n",
-                                       key,
-                                       stat,
-                                       hist.query(quant).unwrap(),
-                                       hist.time,
-                                       fmt_tags(&hist.tags))
-                                    .unwrap()
-                            }
-                            payload_size = payload_size.saturating_add(1);
-                            histogram_payload_size = histogram_payload_size.saturating_add(1);
-                            time_spreads =
-                                time_spreads.insert_value((hist.created_time - now).abs() as f64);
-                            histogram_time_spreads = histogram_time_spreads.insert_value((hist.created_time - now).abs() as f64);
-                            let count = hist.count();
-                            write!(stats,
-                                   "{}.count {} {} {}\n",
-                                   key,
-                                   count,
-                                   hist.time,
-                                   fmt_tags(&hist.tags))
-                                .unwrap();
-                        }
-                    }
+        let mut tag_buf = String::with_capacity(1_024);
+        for (key, vals) in flat_aggrs {
+            for m in vals {
+                if let Some(v) = m.value() {
+                    self.stats.push_str(key);
+                    self.stats.push_str(" ");
+                    self.stats.push_str(&v.to_string());
+                    self.stats.push_str(" ");
+                    self.stats.push_str(&m.time.to_string());
+                    self.stats.push_str(" ");
+                    fmt_tags(&m.tags, &mut tag_buf);
+                    self.stats.push_str(&tag_buf);
+                    self.stats.push_str("\n");
+
+                    tag_buf.clear();
                 }
             }
         }
 
-        for ref spread in &[&time_spreads,
-                            &gauge_time_spreads,
-                            &delta_gauge_time_spreads,
-                            &raw_time_spreads,
-                            &counter_time_spreads,
-                            &timer_time_spreads,
-                            &histogram_time_spreads] {
-            for tup in &[("min", 0.0),
-                         ("max", 1.0),
-                         ("50", 0.5),
-                         ("75", 0.75),
-                         ("95", 0.95),
-                         ("99", 0.99),
-                         ("999", 0.999)] {
-                let stat: &str = tup.0;
-                let quant: f64 = tup.1;
-                write!(stats,
-                       "{}.{} {} {} {}\n",
-                       spread.name,
-                       stat,
-                       spread.query(quant).unwrap(),
-                       now,
-                       fmt_tags(&self.global_tags))
-                    .unwrap()
+        let high_aggrs = self.aggrs.histograms().iter().chain(self.aggrs.timers().iter());
+
+        for (key, hists) in high_aggrs {
+            for hist in hists {
+                fmt_tags(&hist.tags, &mut tag_buf);
+                for tup in &[("min", 0.0),
+                             ("max", 1.0),
+                             ("2", 0.02),
+                             ("9", 0.09),
+                             ("25", 0.25),
+                             ("50", 0.5),
+                             ("75", 0.75),
+                             ("90", 0.90),
+                             ("91", 0.91),
+                             ("95", 0.95),
+                             ("98", 0.98),
+                             ("99", 0.99),
+                             ("999", 0.999)] {
+                    let stat: &str = tup.0;
+                    let quant: f64 = tup.1;
+                    self.stats.push_str(key);
+                    self.stats.push_str(".");
+                    self.stats.push_str(stat);
+                    self.stats.push_str(" ");
+                    self.stats.push_str(&hist.query(quant).unwrap().to_string());
+                    self.stats.push_str(" ");
+                    self.stats.push_str(&hist.time.to_string());
+                    self.stats.push_str(" ");
+                    self.stats.push_str(&tag_buf);
+                    self.stats.push_str("\n");
+                }
+                let count = hist.count();
+                self.stats.push_str(key);
+                self.stats.push_str(".count");
+                self.stats.push_str(" ");
+                self.stats.push_str(&count.to_string());
+                self.stats.push_str(" ");
+                self.stats.push_str(&hist.time.to_string());
+                self.stats.push_str(" ");
+                self.stats.push_str(&tag_buf);
+                self.stats.push_str("\n");
+
+                tag_buf.clear();
             }
-            write!(stats,
-                   "{}.count {} {} {}\n",
-                   spread.name,
-                   spread.count(),
-                   now, 
-                   fmt_tags(&self.global_tags))
-                .unwrap();
         }
-
-        write!(stats,
-               "cernan.{}.delivery_attempts {} {} {}\n",
-               self.sink_name,
-               self.delivery_attempts,
-               now,
-               fmt_tags(&self.global_tags))
-            .unwrap();
-
-        for &(sz, nm) in &[(payload_size, "payload_size"),
-                           (gauge_payload_size, "gauge_payload_size"),
-                           (delta_gauge_payload_size, "delta_gauge_payload_size"),
-                           (raw_payload_size, "raw_payload_size"),
-                           (counter_payload_size, "counter_payload_size"),
-                           (timer_payload_size, "timer_payload_size"),
-                           (histogram_payload_size, "histogram_payload_size")] {
-            write!(stats,
-                   "cernan.{}.{} {} {} {}\n",
-                   self.sink_name,
-                   nm,
-                   sz,
-                   now,
-                   fmt_tags(&self.global_tags))
-                .unwrap();
-        }
-
-        stats
     }
 }
 
@@ -310,13 +138,13 @@ impl Sink for Wavefront {
                 Ok(srv) => {
                     let ips: Vec<_> = srv.collect();
                     for ip in ips {
-                        time::delay(self.delivery_attempts);
                         match TcpStream::connect(ip) {
                             Ok(mut stream) => {
-                                let res = stream.write(self.format_stats(time::now()).as_bytes());
+                                self.format_stats(time::now());
+                                let res = stream.write(self.stats.as_bytes());
                                 if res.is_ok() {
-                                    trace!("flushed to wavefront!");
                                     self.aggrs.reset();
+                                    self.stats.clear();
                                     self.delivery_attempts = 0;
                                     return;
                                 } else {
@@ -332,6 +160,7 @@ impl Sink for Wavefront {
                                       e)
                             }
                         }
+                        time::delay(self.delivery_attempts);
                     }
                 }
                 Err(e) => {
@@ -422,8 +251,8 @@ mod test {
             .time(dt_1)
             .overlay_tags_from_map(&tags)
             .created_time(dt_0));
-        let result = wavefront.format_stats(dt_2);
-        let lines: Vec<&str> = result.lines().collect();
+        wavefront.format_stats(dt_2);
+        let lines: Vec<&str> = wavefront.stats.lines().collect();
 
         println!("{:?}", lines);
         assert!(lines.contains(&"test.counter 1 645181811 source=test-src"));
